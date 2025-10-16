9d538271c3e551b9a2b28bc2b09e539c6b52fba7
henrytitheridge-stone <henrytitheridgestone@gmail.com>
2025-10-16 12:58:49 +0100

    Modify settings.py to retrieve secret key from environment variables

--- codestar/settings.py
+++ codestar/settings.py
@@ -24,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d)w1cqgalb)!5(615o53$e9$52h*t(l%tmq3_)ozj=g@8mnff*'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.herokuapp.com']
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
